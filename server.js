042aee236472faa4357717b0fafaeccc0a6c0b4b
GabrielLaborda <gabriel.laborda.fsd@gmail.com>
2024-08-03 17:48:20 -0300

    Update Cors

--- server.js
+++ server.js
@@ -6,7 +6,12 @@ const connectDB = require("./db");
 
 // Configuracion de la aplicación
 const app = express();
-app.use(cors());
+cors({
+  origin: "https://penca-iota.vercel.app", // Ajusta el origen a tu dominio frontend
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"], // Lista de métodos permitidos
+  credentials: true, // Permite enviar cookies y credenciales
+  allowedHeaders: ["Content-Type", "Authorization"], // Encabezados permitidos
+});
 app.use(express.json());
 
 // Conexión a MongoDB
